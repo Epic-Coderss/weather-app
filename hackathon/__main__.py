c9a8d318549dde680c2e4dc88d12f3c2db631c78
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-07-19 12:43:31 -0700

    Rewrite __main__ to be better

--- hackathon/__main__.py
+++ hackathon/__main__.py
@@ -1,38 +1,12 @@
-import os
-from tkinter import Canvas, Tk
-import tkinter as tk
-import requests
-from PIL import Image, ImageTk
 from io import BytesIO
-import matplotlib.colors as colors
-import numpy as np
+from tkinter import Canvas, Tk
+
+import requests
+from matplotlib import colors
+from PIL import Image, ImageTk
 
 WEATHER_KEY = "d4996d8ccefb306921a70705b6779e2a"
 
-RED = [255,0,0]
-GREEN = [0,255,0]
-BLUE = [0,0,255]
-
-class OpenWeatherMap:
-    APPID = 'c73d9cdb31fd6a386bee66158b116cd0'
-
-    def __init__(self):
-        self.url = "http://api.openweathermap.org/data/2.5/weather?appid={appid}&q={city}&units=metric"
-        self.json = {}
-
-    def get_city(self, city):
-        url = self.url.format(appid=OpenWeatherMap.APPID, city=city)
-        self.json = requests.get(url).json()
-        return self.json
-
-    def get(self, key):
-        return self.json['main'][key]
-
-    def get_icon_data(self):
-        icon_id = self.json['weather'][0]['icon']
-        url = 'http://openweathermap.org/img/wn/{icon}.png'.format(icon=icon_id)
-        response = requests.get(url, stream=True)
-        return response.content
 
 def weather(latitude, longitude, units="imperial"):
     result = requests.get(
@@ -41,7 +15,17 @@ def weather(latitude, longitude, units="imperial"):
         }&lon={longitude}&appid={WEATHER_KEY}&units={units}"""
     ).json()
 
-    return {"temperature": result["main"]["temp"], "icon": result['weather'][0]['icon']}
+    icon = requests.get(
+        f"""http://openweathermap.org/img/wn/{
+            result["weather"][0]["icon"]
+        }@4x.png"""
+    )
+
+    return {
+        "temperature": result["main"]["temp"],
+        "icon": icon.content,
+    }
+
 
 def coordinates():
     result = requests.get("https://ipinfo.io/").json()["loc"].split(",")
@@ -50,61 +34,63 @@ def coordinates():
 
 
 class App(object):
-    def __init__(self, dimensions=(200, 200)):
+    def __init__(self, dimensions=(150, 150)):
         self._window = Tk()
         self._dimensions = dimensions
 
-        self._window.geometry(f"{dimensions[0]}x{dimensions[1]}")
+        self._window.geometry(f"{self._dimensions[0]}x{self._dimensions[1]}")
         self._window.resizable(False, False)
+        self._window.title("Weather")
 
         self._canvas = Canvas(self._window)
         self._canvas.pack(expand=True, fill="both")
-        
-        owm = OpenWeatherMap()
-        owm.get_city('Fremont,CA,USA')
-
-        temperature = owm.get('temp') # this doesn't do anything right now but I left it
-        
-        icon = owm.get_icon_data()
-        img = Image.open(BytesIO(icon)) # this opens normal PIL Image
-        img = ImageTk.PhotoImage(img) # this opens ImageTk from earlier image
 
         self._milliseconds = 60000
+        # cache coordinates
         self._coordinates = coordinates()
 
-        self._update(img) # update gets the image created earlier
+        # property for keeping a reference to original image
+        self._icon = None
+
+        self._update()
         self._window.mainloop()
 
-    def _update(self, img):
-        temperature = weather(*self._coordinates)["temperature"]
-        low_temp = 55
-        high_temp = 350
-        r = temperature
-        g = 0
-        b = temperature
+    def _update(self):
+        weather_result = weather(*self._coordinates)
 
-        r = max(min(high_temp, temperature), low_temp) # makes sure that r is within low_temp - high_temp range
-        b = max(min(high_temp, temperature), low_temp) # makes sure that r is within low_temp - high_temp range
-        r = (r - low_temp) / (high_temp - low_temp)
-        b = 1.0 - ((b - low_temp) / (high_temp - low_temp))
+        temperature = weather_result["temperature"]
 
-        hex = colors.rgb2hex((r, g, b))
-        print(str(r) + ' ' + str(g) + ' ' + str(b) + ' ')
-        print(hex)
-        print(temperature)
+        low_temperature = 0
+        high_temperature = 100
+
+        red = max(min(high_temperature, temperature), low_temperature)
+        green = 0
+        blue = max(min(high_temperature, temperature), low_temperature)
+
+        red = (red - low_temperature) / (high_temperature - low_temperature)
+        blue = 1.0 - (
+            (blue - low_temperature) / (high_temperature - low_temperature)
+        )
+
+        final_hex = colors.rgb2hex((red, green, blue))
+        icon_size = min(self._dimensions)
+
+        # use property to prevent garbage collection of icon before display
+        self._icon = BytesIO(weather_result["icon"])
+        self._icon = Image.open(self._icon)
+        self._icon = self._icon.resize((icon_size, icon_size))
+        self._icon = ImageTk.PhotoImage(self._icon)
+
         self._canvas.delete("all")
 
-        if 70 <= temperature <= 80:
-            self._canvas.create_rectangle(0, 0, *self._dimensions, fill=hex)
-        elif 65 <= temperature <= 85:
-            self._canvas.create_rectangle(
-                0, 0, *self._dimensions, fill=hex
-            )
-        else:
-            self._canvas.create_rectangle(0, 0, *self._dimensions, fill=hex)
-        xpos = 70 # this controls where the image appears on canvas in respect to X axis
-        ypos = 80 # this controls where the image appears on canvas in respect to Y axis
-        self._canvas.create_image((xpos, ypos), image=img, anchor='nw') # after canvas' color was updated we are drawing the image on it
+        self._canvas.create_rectangle(0, 0, *self._dimensions, fill=final_hex)
+
+        self._canvas.create_image(
+            (self._dimensions[0] / 2) - (icon_size / 2),
+            (self._dimensions[1] / 2) - (icon_size / 2),
+            image=self._icon,
+            anchor="nw",
+        )
 
         self._window.after(self._milliseconds, self._update)
 
