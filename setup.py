6ad0e2f78414b0389f2508c8b732b02764b3a2b4
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2020-07-19 16:21:29 -0700

    Fix links

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    "url": "https://github.com/HereIsKevin/hackathon",
-    "download_url": "https://github.com/HereIsKevin/hackathon",
+    "url": "https://github.com/shriraj20/weather-app",
+    "download_url": "https://github.com/shriraj20/weather-app.",
     "version": "0.1.0",
     "install_requires": ["requests", "pillow"],
     "packages": ["hackathon"],
